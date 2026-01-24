dc61e4b251ae0e6f01000e0e28d5cec67c849eb0
Nek-12 <46318640+Nek-12@users.noreply.github.com>
2026-01-24 16:54:42 +0000

    ksrc v0.5.1

--- Formula/ksrc.rb
+++ Formula/ksrc.rb
@@ -1,8 +1,8 @@
 class Ksrc < Formula
   desc "One-liner search and read for Kotlin dependency sources"
   homepage "https://github.com/respawn-app/ksrc"
-  url "https://github.com/respawn-app/ksrc/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "4660b65250bb25639e813fdf7dacbf22868df461797d001717d8ea51c985a05f"
+  url "https://github.com/respawn-app/ksrc/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "80a081b2267a08b85c9a2bd01ca77f69a51399990a06e314ec917bb1eda57773"
   license "AGPL-3.0-only"
   revision 1
 
