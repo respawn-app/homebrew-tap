132e424346ff39acc73a2640353e2d0d0a7c75a8
Nek-12 <46318640+Nek-12@users.noreply.github.com>
2026-01-31 13:18:28 +0000

    ksrc v0.5.2
    Closes #3.
    
    Signed-off-by: Nek.12 <46318640+Nek-12@users.noreply.github.com>

--- Formula/ksrc.rb
+++ Formula/ksrc.rb
@@ -1,15 +1,9 @@
 class Ksrc < Formula
   desc "One-liner search and read for Kotlin dependency sources"
   homepage "https://github.com/respawn-app/ksrc"
-  url "https://github.com/respawn-app/ksrc/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "80a081b2267a08b85c9a2bd01ca77f69a51399990a06e314ec917bb1eda57773"
+  url "https://github.com/respawn-app/ksrc/archive/refs/tags/v0.5.2.tar.gz"
+  sha256 "8fb12e308775e5249b3c6b7ca888b658834ede75136f6f3ccff948b1b7a56707"
   license "Apache-2.0"
-  bottle do
-    root_url "https://ghcr.io/v2/respawn-app/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "62f87d222eb2222068613c80f84ed8f04e889c53e34fc82824446b8e5db34d6a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "673add76551685ddcfdd15f625b113792f5c9d58319b2f2352b91007dfc12708"
-  end
-
   depends_on "go" => :build
   depends_on "ripgrep"
 
