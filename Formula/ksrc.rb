bd920f40c40b3254219f6d5c5dfece493e21704c
Nek.12 <46318640+Nek-12@users.noreply.github.com>
2026-02-01 13:30:54 +0000

    ksrc: add 0.6.0 bottle.

--- Formula/ksrc.rb
+++ Formula/ksrc.rb
@@ -4,6 +4,12 @@ class Ksrc < Formula
   url "https://github.com/respawn-app/ksrc/archive/refs/tags/v0.6.0.tar.gz"
   sha256 "d17e1f54162e1605e176e4a64c7dc7f317da112b508b408f4d55256d482ee830"
   license "Apache-2.0"
+
+  bottle do
+    root_url "https://ghcr.io/v2/respawn-app/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "7b677451f52bafc94426360f0e4a136134c1133ace733ea6cb2995467244ef1d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "34278ce92023c1c7bda32857e9719be2a91269cc7e82e05b261eee5dc40be784"
+  end
   depends_on "go" => :build
   depends_on "ripgrep"
 
