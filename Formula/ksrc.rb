ba40c1fbc3f5f6afbaaab55493631810ed75afd4
Nek-12 <vaizin.nikita@gmail.com>
2026-02-01 14:06:49 +0100

    ksrc: drop revision

--- Formula/ksrc.rb
+++ Formula/ksrc.rb
@@ -4,7 +4,6 @@ class Ksrc < Formula
   url "https://github.com/respawn-app/ksrc/archive/refs/tags/v0.6.0.tar.gz"
   sha256 "d17e1f54162e1605e176e4a64c7dc7f317da112b508b408f4d55256d482ee830"
   license "Apache-2.0"
-  revision 1
   test do
     assert_match "ksrc", shell_output("#{bin}/ksrc --help")
   end
