7ec4b6b70d3231e4cf61e15451fc9e124b5c0339
Nek.12 <46318640+Nek-12@users.noreply.github.com>
2026-03-23 12:16:59 +0000

    builder-cli: update 0.1 bottle.

--- Formula/builder-cli.rb
+++ Formula/builder-cli.rb
@@ -6,7 +6,10 @@ class BuilderCli < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    root_url "https://ghcr.io/v2/respawn-app/homebrew-tap"
+    root_url "https://ghcr.io/v2/respawn-app/tap"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "3b3a91c7ea97eececb6a096fb970390b4c51bdea8b2c64f88809782b817e5a12"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3f71b67e846b3aa449b3ec4d61440d7fb61aa68959b49a66d69ef2959413ba6d"
   end
 
   depends_on "go" => :build
