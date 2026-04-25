a943dfaddd5f7cf249552ee09a55aefcd081ef23
Nek-12 <46318640+Nek-12@users.noreply.github.com>
2026-04-25 15:24:15 +0000

    builder-cli v1.0.0
    Closes #24.
    
    Signed-off-by: Nek.12 <46318640+Nek-12@users.noreply.github.com>

--- Formula/builder-cli.rb
+++ Formula/builder-cli.rb
@@ -1,14 +1,12 @@
 class BuilderCli < Formula
   desc "Minimal terminal coding agent for professional engineering workflows"
   homepage "https://github.com/respawn-app/builder"
-  url "https://github.com/respawn-app/builder/archive/refs/tags/v0.10.1.tar.gz"
-  sha256 "ed108d7b8c85f9f1adbd5b640ac8083001edf6063c5337369262a0f8eb0a350d"
+  url "https://github.com/respawn-app/builder/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "4ef090548128003d7cb09f9d8c1ae454ce33847de396c885880f1af5c43dad7f"
   license "AGPL-3.0-only"
 
   bottle do
     root_url "https://ghcr.io/v2/respawn-app/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "4b239cddaf52db90a33582253bbe63a09b6a8d367b86da15d9c6ea41ddb91b14"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a57a41f3392507ed5a6652b87ee7e4270a945e7128181ece1d3c67b672ebb579"
   end
 
   depends_on "go" => :build
