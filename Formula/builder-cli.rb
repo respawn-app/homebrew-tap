25fee7e3f2b50ebabc579ed33c72416e2682ee73
Nek.12 <46318640+Nek-12@users.noreply.github.com>
2026-05-01 23:19:48 +0000

    builder-cli: update 1.1.1 bottle.

--- Formula/builder-cli.rb
+++ Formula/builder-cli.rb
@@ -7,6 +7,8 @@ class BuilderCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/respawn-app/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "60a282cd9e5d51b839ee793f70de22092f382e27345acab211050732a5a34f2c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c5a6d2bca197584a37bc9af2d98a47425b2f57ed48eae54ef45e316270e3eb7f"
   end
 
   depends_on "go" => :build
