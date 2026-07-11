9a6ad028dcc0cf1acdf72878e883240f6366c005
Nek-12 <46318640+Nek-12@users.noreply.github.com>
2026-07-11 21:00:08 +0000

    kent v2.2.0
    Closes #39.
    
    Signed-off-by: Nek.12 <46318640+Nek-12@users.noreply.github.com>

--- Formula/kent.rb
+++ Formula/kent.rb
@@ -1,14 +1,11 @@
 class Kent < Formula
   desc "Minimal terminal coding agent for professional engineering workflows"
   homepage "https://github.com/respawn-llc/kent"
-  version "2.1.1"
+  version "2.2.0"
   license "AGPL-3.0-only"
 
   bottle do
     root_url "https://ghcr.io/v2/respawn-llc/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "49ef131ff9c2634b98d124c5bb2ad963599cd07cb557ca6883ae8bf5d6e86c27"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "24612cc9ab684dd288b6b1ada4006ad1f4a22266b0144cf0d559f744de229e4a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1a39a5aee856d6950fcd6c07cd398093eddd77e010e4f8bd697811a13ad20af0"
   end
 
   depends_on "ripgrep"
@@ -17,19 +14,19 @@ class Kent < Formula
     depends_on arch: :arm64
 
     on_arm do
-      url "https://github.com/respawn-llc/kent/releases/download/v2.1.1/kent_2.1.1_darwin_arm64.tar.gz"
-      sha256 "9db5b59ca3275cfaa9e0ba331c3ad7ef52f615b5060f939717f932eaa9f11c71"
+      url "https://github.com/respawn-llc/kent/releases/download/v2.2.0/kent_2.2.0_darwin_arm64.tar.gz"
+      sha256 "a8face4131ba238bf00dd3568530e97040e1be21b4b0d2e8b8c602ea50891443"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/respawn-llc/kent/releases/download/v2.1.1/kent_2.1.1_linux_arm64.tar.gz"
-      sha256 "2967ab414643073d70b63643bb8b88e64429a1d85a992db27006eaa65fb87b99"
+      url "https://github.com/respawn-llc/kent/releases/download/v2.2.0/kent_2.2.0_linux_arm64.tar.gz"
+      sha256 "bb1fc27c844a2914a060841d8887c41d41972ea96bd11e45547d280c1b7c2029"
     end
     on_intel do
-      url "https://github.com/respawn-llc/kent/releases/download/v2.1.1/kent_2.1.1_linux_amd64.tar.gz"
-      sha256 "c9be61c9e22fe4d7c3ff0a2e9e66e4e44a67982e7bbeb0604fd4426491df82b1"
+      url "https://github.com/respawn-llc/kent/releases/download/v2.2.0/kent_2.2.0_linux_amd64.tar.gz"
+      sha256 "b95f950be0a2653bb8902ebc4c796636c0e00a6109f516f08e49073ee621c96c"
     end
   end
 
