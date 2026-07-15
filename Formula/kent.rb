e098ba3dd48d6caca3c1bb4589a84a484ec2a3c8
Nek-12 <46318640+Nek-12@users.noreply.github.com>
2026-07-15 10:02:00 +0000

    kent v2.3.0

--- Formula/kent.rb
+++ Formula/kent.rb
@@ -1,14 +1,11 @@
 class Kent < Formula
   desc "Minimal terminal coding agent for professional engineering workflows"
   homepage "https://github.com/respawn-llc/kent"
-  version "2.2.0"
+  version "2.3.0"
   license "AGPL-3.0-only"
 
   bottle do
     root_url "https://ghcr.io/v2/respawn-llc/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "6d7ec5cafdac8ea68d8525601aaf98af5040a09771f59c96e13ec199d4c48d3d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "cb863d254ab97dfd1f60d368c67a68080ca75f9e29c974af4be6adb665f99228"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "947af8ec988771555b69ee77f6ac4d0ab60071203b121de56360bdf4f9953467"
   end
 
   depends_on "ripgrep"
@@ -17,19 +14,19 @@ class Kent < Formula
     depends_on arch: :arm64
 
     on_arm do
-      url "https://github.com/respawn-llc/kent/releases/download/v2.2.0/kent_2.2.0_darwin_arm64.tar.gz"
-      sha256 "a8face4131ba238bf00dd3568530e97040e1be21b4b0d2e8b8c602ea50891443"
+      url "https://github.com/respawn-llc/kent/releases/download/v2.3.0/kent_2.3.0_darwin_arm64.tar.gz"
+      sha256 "6b1abf2ea5e24f90d80780f289923580a701526974d63b836b5eea397d6da719"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/respawn-llc/kent/releases/download/v2.2.0/kent_2.2.0_linux_arm64.tar.gz"
-      sha256 "bb1fc27c844a2914a060841d8887c41d41972ea96bd11e45547d280c1b7c2029"
+      url "https://github.com/respawn-llc/kent/releases/download/v2.3.0/kent_2.3.0_linux_arm64.tar.gz"
+      sha256 "6f5876654adcfe226dd6ae4e62e397192026bbf8753d4bcf2395b04619389346"
     end
     on_intel do
-      url "https://github.com/respawn-llc/kent/releases/download/v2.2.0/kent_2.2.0_linux_amd64.tar.gz"
-      sha256 "b95f950be0a2653bb8902ebc4c796636c0e00a6109f516f08e49073ee621c96c"
+      url "https://github.com/respawn-llc/kent/releases/download/v2.3.0/kent_2.3.0_linux_amd64.tar.gz"
+      sha256 "692215787455990dccd44f665c87f84bb63fe54d4f797ecb3d33d12ad0ca77ce"
     end
   end
 
